b51e1146d809a6d6a647702fb45acc4427dd0d69
Sheldon Smickley <sheldon.smickley@gmail.com>
2023-12-25 22:04:46 -0500

    updated prizes

--- components/Registry.js
+++ components/Registry.js
@@ -4,7 +4,7 @@ const Registry = ({ cases }) => {
   const unrevealedCases = cases.filter(caseItem => !caseItem.isOpen);
 
   return (
-    <div className="bg-black text-white shadow rounded p-4">
+    <div className="bg-black text-white shadow rounded p-4 m-2">
       <ul>
         {unrevealedCases.map((caseItem, index) => (
           <li key={index} className={`border-b border-gray-200 py-2 ${caseItem.isOpen ? 'line-through text-red-500' : ''}`}>
